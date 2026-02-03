279c187b174f0d54a266bda32ee7b21b7a465527
Kobi Hari <kobi2294@yahoo.com>
2026-02-03 18:50:53 +0200

    S02L03 - Using ElementRef

--- demo/01. fun-with-directives/src/app/directives/highlight.directive.ts
+++ demo/01. fun-with-directives/src/app/directives/highlight.directive.ts
@@ -1,11 +1,14 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, inject } from "@angular/core";
 
 @Directive({
     selector: '[highlight]'
 })
 export class HighlightDirective {
+    readonly hostElement = inject<ElementRef<HTMLElement>>(ElementRef);
+
     constructor() {
         console.log('Highlight Directive was created');
+        this.hostElement.nativeElement.style.backgroundColor = 'yellow';
     }
 
 }
