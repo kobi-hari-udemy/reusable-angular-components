4427402a90ed50fe7ca6733b73ba56dc526dfce8
Kobi Hari <kobi2294@yahoo.com>
2026-02-03 19:12:39 +0200

    S02L05 - Using the Renderer2 Service

--- demo/01. fun-with-directives/src/app/directives/highlight.directive.ts
+++ demo/01. fun-with-directives/src/app/directives/highlight.directive.ts
@@ -1,14 +1,20 @@
-import { Directive, ElementRef, inject } from "@angular/core";
+import { Directive, ElementRef, inject, Renderer2 } from "@angular/core";
 
 @Directive({
     selector: '[highlight]'
 })
 export class HighlightDirective {
-    readonly hostElement = inject<ElementRef<HTMLElement>>(ElementRef);
+    readonly hostElement = inject(ElementRef);
+    readonly renderer = inject(Renderer2);
 
     constructor() {
         console.log('Highlight Directive was created');
-        this.hostElement.nativeElement.style.backgroundColor = 'yellow';
+        // this.hostElement.nativeElement.style.backgroundColor = 'yellow';
+
+        this.renderer.setStyle(this.hostElement.nativeElement, 
+            'background-color', 
+            'pink'        
+        );
     }
 
 }
