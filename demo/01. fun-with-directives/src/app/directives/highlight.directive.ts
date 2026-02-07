ad522e7c6dc352ec2b20f964087c42026436fea4
Kobi Hari <kobi2294@yahoo.com>
2026-02-07 12:50:12 +0200

    S02L07 - Binding to Style Properties

--- demo/01. fun-with-directives/src/app/directives/highlight.directive.ts
+++ demo/01. fun-with-directives/src/app/directives/highlight.directive.ts
@@ -3,11 +3,17 @@ import { Directive, ElementRef, inject, Renderer2, signal } from "@angular/core"
 @Directive({
     selector: '[highlight]', 
     host: {
-        '[style.background-color]': 'bg()'
+        '[style.background-color]': 'bg()', 
+        '[style.border]': '"0px solid blue"', 
+        '[style.border-bottom-width.px]': 'thickness()', 
+        '[style.--my-property.pt]': 'thickness()'
     }
 })
 export class HighlightDirective {
     readonly bg = signal('lime');
+
+    readonly thickness = signal(3);
+
     constructor() {
         setTimeout(() => {
             this.bg.set('pink')
