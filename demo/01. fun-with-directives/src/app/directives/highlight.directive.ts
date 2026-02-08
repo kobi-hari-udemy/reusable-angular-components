be6590b9313cfb0b7a7872c30fb5d4ffe54b055c
Kobi Hari <kobi2294@yahoo.com>
2026-02-08 14:13:43 +0200

    S02L10 - Host Listener Arguments

--- demo/01. fun-with-directives/src/app/directives/highlight.directive.ts
+++ demo/01. fun-with-directives/src/app/directives/highlight.directive.ts
@@ -3,13 +3,19 @@ import { computed, Directive, ElementRef, inject, Renderer2, signal } from '@ang
 @Directive({
   selector: '[highlight]',
   host: {
+    '[attr.tabindex]': '0',
     '[style.background-color]': 'bg()',
     '[style.cursor]': '"pointer"',
-    '(click)': 'changeColor()'
+    '(click)': 'changeColor()',
+    '(keyup.shift.enter)': 'onKeyPressed($event)',
   },
 })
 export class HighlightDirective {
   readonly bg = signal('lime');
+
+  onKeyPressed(ev: Event) {
+    console.log('A key was pressed', ev);
+  }
 
   changeColor() {
     this.bg.set('pink');
