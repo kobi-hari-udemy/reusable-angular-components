9cb410823363dc5de596a2a50c54dc640db9da2e
Kobi Hari <kobi2294@yahoo.com>
2026-02-12 18:44:32 +0200

    S02L17 - Track Mouse Challange Solution

--- practice/03. track-mouse-directive/src/app/directives/track-mouse.directive.ts
+++ practice/03. track-mouse-directive/src/app/directives/track-mouse.directive.ts
@@ -1,7 +1,30 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, inject, signal } from '@angular/core';
+import { clamp } from './helpers';
 
 @Directive({
   selector: '[track-mouse]',
+  host: {
+    '(mousemove)': 'mouseMoved($event)', 
+    '[style.--mouse-x.px]': 'x()', 
+    '[style.--mouse-y.px]': 'y()'
+  }, 
+  exportAs: 'track-mouse'
 })
 export class TrackMouse {
+  readonly x = signal(0);
+  readonly y = signal(0);
+
+  readonly elem = inject(ElementRef).nativeElement as HTMLElement;
+
+  mouseMoved(ev: MouseEvent) {
+    const rect = this.elem.getBoundingClientRect();
+    const x = clamp(0, ev.clientX - rect.left, rect.width);
+    const y = clamp(0, ev.clientY - rect.top, rect.height);
+
+    this.x.set(x);
+    this.y.set(y);
+
+    console.log(x, y);
+
+  }
 }
