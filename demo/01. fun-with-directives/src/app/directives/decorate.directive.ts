ed2aeee0fc7242fa75067dc6fac470fe41b13251
Kobi Hari <kobi2294@yahoo.com>
2026-02-15 14:32:44 +0200

    S02L18 - Directive Composition

--- demo/01. fun-with-directives/src/app/directives/decorate.directive.ts
+++ demo/01. fun-with-directives/src/app/directives/decorate.directive.ts
@@ -1,9 +1,22 @@
 import {
   Directive,
+  input,
 } from '@angular/core';
+import { HighlightDirective } from './highlight.directive';
+import { UnderlineDirective } from './underline.directive';
 
 @Directive({
   selector: '[decorate]',
+  hostDirectives: [
+    UnderlineDirective, 
+    {
+      directive: HighlightDirective, 
+      inputs: [
+        'highlight: decorate'
+      ]
+      
+    }
+  ]
 })
 export class DecorateDirective {
 }
