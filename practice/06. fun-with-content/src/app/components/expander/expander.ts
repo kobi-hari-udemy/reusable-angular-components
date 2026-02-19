64634acd80a874a6b80601e30d7b93c5099583d4
Kobi Hari <kobi2294@yahoo.com>
2026-02-19 19:24:09 +0200

    section 4 starting point

---
+++ practice/06. fun-with-content/src/app/components/expander/expander.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expander',
+  imports: [],
+  templateUrl: './expander.html',
+  styleUrl: './expander.scss',
+})
+export class ExpanderComponent {
+
+}
