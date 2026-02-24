864cdc044f83e2823a2896299c86952e14e6c919
Kobi Hari <kobi2294@yahoo.com>
2026-02-24 22:08:35 +0200

    S04L07 - Content Selection using Attributes

--- demo/05. fun-with-content/src/app/app.ts
+++ demo/05. fun-with-content/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ExpanderComponent } from './components/expander/expander';
+import { Icon } from './components/icon/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [ExpanderComponent],
+  imports: [ExpanderComponent, Icon],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
