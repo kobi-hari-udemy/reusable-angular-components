7cbaa368f5ed26e22cb1f493a2973b8102ee3104
Kobi Hari <kobi2294@yahoo.com>
2026-02-26 12:33:59 +0200

    S04L14 - Adding fallback toggle using contentChild

--- demo/05. fun-with-content/src/app/components/expander/expander.component.ts
+++ demo/05. fun-with-content/src/app/components/expander/expander.component.ts
@@ -1,4 +1,5 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, contentChild, signal } from '@angular/core';
+import { ExpanderToggleComponent } from './expander-toggle/expander-toggle.component';
 
 @Component({
   selector: 'app-expander',
@@ -16,6 +17,9 @@ export class ExpanderComponent {
 
   readonly isExpanded = this.#isExpanded.asReadonly();
   readonly isCollapsed = computed(() => !this.isExpanded());
+
+  readonly expanderToggleComponent = contentChild(ExpanderToggleComponent);
+  readonly hasCustomToggle = computed(() => !!this.expanderToggleComponent());
 
   toggle() {
     this.#isExpanded.update(v => !v);
