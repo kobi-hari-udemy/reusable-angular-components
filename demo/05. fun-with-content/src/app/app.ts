adb1651d3cfaa9502b4f01058dc24ab6231ead14
Kobi Hari <kobi2294@yahoo.com>
2026-03-04 13:06:20 +0200

    S05L02

--- demo/05. fun-with-content/src/app/app.ts
+++ demo/05. fun-with-content/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Icon } from './components/icon/icon';
 import { Expander } from './components/expander/expander';
+import { Blank } from './components/blank/blank';
 
 @Component({
   selector: 'app-root',
-  imports: [Expander, Icon],
+  imports: [Expander, Icon, Blank],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ demo/05. fun-with-content/src/app/components/blank/blank.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-blank',
+  imports: [],
+  templateUrl: './blank.html',
+  styleUrl: './blank.scss',
+  encapsulation: ViewEncapsulation.None
+})
+export class Blank {
+
+}
