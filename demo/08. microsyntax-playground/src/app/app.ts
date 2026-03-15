8e7a52d3a7c2d6a82f0060a04b0d6a06f839b60e
Kobi Hari <kobi2294@yahoo.com>
2026-03-15 14:03:36 +0200

    code for s8

---
+++ demo/08. microsyntax-playground/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { MyFor } from './directives/my-for.directive';
+import { MyIf } from './directives/my-if.directive';
+import { MyRepeat } from './directives/my-repeat.directive';
+import { MyTimer } from './directives/my-timer.directive';
+
+@Component({
+  selector: 'app-root',
+  imports: [MyFor, MyIf, MyRepeat, MyTimer],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+}
