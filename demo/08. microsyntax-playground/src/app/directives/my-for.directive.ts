f6dacc7a4aa493f6ba32d6c4b0de2c28deb85849
Kobi Hari <kobi2294@yahoo.com>
2026-03-15 19:38:40 +0200

    S08L09 - The legacy *ngIf and *ngFor

--- demo/08. microsyntax-playground/src/app/directives/my-for.directive.ts
+++ demo/08. microsyntax-playground/src/app/directives/my-for.directive.ts
@@ -3,20 +3,21 @@ import { Directive, input } from "@angular/core";
 export interface MyForContext {
     readonly $implicit: any;
     readonly index: number;
-    readonly last: boolean;
     readonly first: boolean;
+    readonly last: boolean;
     readonly odd: boolean;
     readonly even: boolean;
 }
 
 export type MyForTrackBy = (item: any, index: number) => any;
 
+
 @Directive({
     selector: '[myFor]'
 })
 export class MyFor {
-    readonly myIfOf = input.required<any[]>;
-    readonly myIfTrackBy = input<MyForTrackBy>((item, index) => item);
+    readonly myForOf = input.required<any[]>();
+    readonly myForTrackBy = input<MyForTrackBy>((item, index) => item);
 
     static ngTemplateContextGuard(_: MyFor, ctx: unknown): ctx is MyForContext {
         return true;
