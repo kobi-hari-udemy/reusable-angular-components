d39faafaae875b97b6acb3f44647c52e171019a8
Kobi Hari <kobi2294@yahoo.com>
2026-03-18 15:57:00 +0200

    fix

--- practice/13. microsyntax-playground-myif/src/app/app.ts
+++ practice/13. microsyntax-playground-myif/src/app/app.ts
@@ -1,14 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { MyFor } from './directives/my-for.directive';
 import { MyIf } from './directives/my-if.directive';
-import { MyRepeat } from './directives/my-repeat.directive';
-import { MyTimer } from './directives/my-timer.directive';
 import { CommonModule } from '@angular/common';
 import { Point } from './models/point';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, MyFor, MyIf, MyRepeat, MyTimer],
+  imports: [CommonModule, MyIf],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
